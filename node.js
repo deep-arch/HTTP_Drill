c0270eb13e61d1cf10d5fff122f96cb10f909f67
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-09-22 13:50:12 +0530

    Fix uuid related bug

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
-const uuid = { uuid: uuidv4 };
+const uuid = { uuid: uuidv4() };
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello There!</h1>");
